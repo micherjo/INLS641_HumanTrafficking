d4f75b229cd2e07c7e579a250eedf5297739a90f
Michelle Erin Johnson <mej@unc.edu>
2020-11-19 20:21:55 -0500

    Update vis-updated.js

--- vis-updated.js
+++ vis-updated.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * INLS 690 - Human Trafficking FBI Data Visualization
+ * INLS 641 - Human Trafficking FBI Data Visualization
  * Laura Haller
  * Michelle Erin Johnson
  * Stacy MacDonald
@@ -14,9 +14,9 @@
  * @return Promise
  */
 Promise.all([
-    d3.json("us-states.json"),
-    d3.csv("statesformap.csv"),
-    d3.csv("human_trafficking.csv")
+    d3.json("data/us-states.json"),
+    d3.csv("data/statesformap.csv"),
+    d3.csv("data/human_trafficking.csv")
 ])
     .then(ready);
 
@@ -63,15 +63,41 @@ function renderMap(data, svg_id, val_range, rate_type) {
     let svg = d3.select(svg_id);
     let colormap = d3.scaleSequentialLog().domain([1, 400]).interpolator(d3.interpolateYlGnBu);
 
+    var colorLegend = d3.legendColor()
+        .shapeWidth(30)
+        .orient('horizontal')
+        .scale(colormap)
+        .cells([0,5,25,125,600])
+        .labels(['0','5','25','125','600'])
+        .shapePadding(5)
+        .shapeWidth(50)
+        .shapeHeight(20)
+        .labelOffset(8);
+
+    svg.append("g")
+        .attr("class", "legendLinear")
+        .attr("transform", "translate(590, -5)");
+
+
+    svg.select(".legendLinear")
+        .call(colorLegend);
+
     // Define the tool tip to use for mouseovers.
     let tool_tip = d3.tip()
         .attr("class", "d3-tip")
         .offset([-8, 0])
         .html(function (d) {
+            if (getrate(stats, d.properties.name, rate_type) === undefined) {
+                let html = "<table>"
+                + "<tr>State Name: </td>" + d.properties.name +"</td></tr>"
+                + "<tr><th>Total Offenses:</th><td>" + "No data available" +"</td></tr>"
+            return html
+            } else{
             let html = "<table>"
                 + "<tr>State Name: </td>" + d.properties.name +"</td></tr>"
                 + "<tr><th>Total Offenses:</th><td>" + getrate(stats, d.properties.name, rate_type);  +"</td></tr>"
             return html
+            }
         });
     svg.call(tool_tip)
 
@@ -94,8 +120,9 @@ function renderMap(data, svg_id, val_range, rate_type) {
  * @param data
  */
 function selected(d) {
-    //set initial category to Age Group
-    var selectedCategory = "Age Group"
+    //set category
+    var selectedCategory = d3.select('input[name="btn"]:checked').property("value");
+    //selectedCategory = "Age Group";
 
     if (!selectedStates.includes(d.properties.name)) {
         d3.select(this).classed("selected", true).raise();
@@ -108,7 +135,7 @@ function selected(d) {
     else {
         d3.select(this).classed("selected", false);
         var index = selectedStates.indexOf(d.properties.name);
-        var state_removed = d.properties.name
+        var state_removed = d.properties.name.replace(" ", "-")
         console.log(state_removed)
         d3.selectAll("#"+ state_removed).remove()
         //remove selected state from selectedStates array
@@ -121,6 +148,10 @@ function selected(d) {
  * Responsible for adding new svg elements to the page for each state selected.
  * @param selectedStates, selectedCategory
  */
+
+ // set the colour scale
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
 function updateGraphs(selectedStates, selectedCategory){
 
     //Set current state to the last state contained in the selectedStates array
@@ -128,8 +159,6 @@ function updateGraphs(selectedStates, selectedCategory){
 
     //Filter statesformap.csv data set to the selected category and the current state
     statedata = data[2].filter(function(d) {return d.category == selectedCategory && d.locationdesc == currentState ;},);
-    //console.log ("state data for selected states: ")
-    //console.log(statedata)
 
     var margin_x = 20;
     var margin_y = 20;
@@ -143,15 +172,16 @@ function updateGraphs(selectedStates, selectedCategory){
     var y = d3.scaleLinear().range([height, 0]).domain([0, 400]);
 
     //D3 Enter Selection: Add svg element of id #state-graphs  for each current state.
+    if(selectedStates.length > 0){
     var svg = d3.select("#state-graphs")
         .append("svg")
-        .attr("id", currentState)
+        .attr("id", currentState.replace(" ", "-"))
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         //what does this do?
         .append("g")
         .attr("transform","translate(" + margin.left + "," + margin.top + ")");
-
+    }
     //the exit selection
     d3.select("#state-graphs")
         .selectAll("svg")
@@ -159,7 +189,6 @@ function updateGraphs(selectedStates, selectedCategory){
         .exit()
         .remove();
 
-    //what does this do?
     let g = svg.append("g")
         .attr("transform", "translate("+margin_x+", "+margin_y+")");
 
@@ -181,6 +210,31 @@ function updateGraphs(selectedStates, selectedCategory){
         .attr("text-anchor", "middle")
         .text(currentState);
 
+    g.append("text")
+        .attr("transform", "rotate(270)")
+        .attr("class", "axis-label")
+        .attr("y", -50)
+        .attr("x", -80)
+        .style("text-anchor", "middle")
+        .text("Number of Cases Per 10M People");
+
+    g.append("text")
+        .attr("class", "axis-label")
+        .attr("y", 200)
+        .attr("x", 175)
+        .style("text-anchor", "middle")
+        .text("Year");
+
+    //display message if there is no data
+    if (statedata.length == 0){
+        g.append("text")
+        .attr("class", "no-data")
+        .attr("x", 320 / 2)
+        .attr("dy", "5em")
+        .attr("text-anchor", "middle")
+        .text("No data reported for this category");
+    }
+
     // Define a d3 line called valueline based on avg_data_values
     var valueline = d3.line()
         .x(function(d) { return x(d.year); })
@@ -192,59 +246,66 @@ function updateGraphs(selectedStates, selectedCategory){
         .key(function(d) {return d.category_value;})
         .entries(statedata);
 
-    // set the colour scale
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    // spacing for the legend
-    legendSpace = width/dataNest.length;
 
     // Loop through each key
     dataNest.forEach(function(d) {
-        //  console.log(d);
         svg.append("path")
             .attr("class", "line")
             .style("stroke", function() {return d.color = color(d.key)})
             .attr("d", valueline(d.values))
             .attr("id","current_factor");
 
-        //filter the category values for the legend
-        let array_of_categories_for_state = statedata.filter(function(d) {return d.locationdesc === currentState;})
+            legendSpace = width/dataNest.length; // spacing for the legend
+
+            //filter the category values for the legend
+            let array_of_categories_for_state = statedata.filter(function(d) {return d.locationdesc === currentState;})
             .map(function(d) { return d.category_value});
 
-        //Make a unique list of the category values to remove repeated values
-        let unique_category_set = new Set(array_of_categories_for_state);
-        let unique_category_array = [...unique_category_set]
+            //Make a unique list of the category values to remove repeated values
+            let unique_category_set = new Set(array_of_categories_for_state);
+            let unique_category_array = [...unique_category_set]
 
-        //Add in the legend data with category values
-        var legend = svg.selectAll("g.legend")
-            .data(unique_category_array)
-            .enter()
-            .append("g")
-            .attr("class", "legend")
-            .attr("id","current_legend");
+            //Add in the legend data with category values
+            var legend = d3.select("#"+ currentState.replace(" ", "-")).select("g").selectAll("g.legend")
+                   .data(unique_category_array)
+                   .enter()
+                   .append("g")
+                   .attr("class", "legend")
+                   .attr("id","current_legend");
 
-        legend.append("text")
-            .attr("x", width + margin.right - 70 - 0)
-            .attr("y", function (d, i) {return (i * 20) + 9;})
-            .data(unique_category_array)
-            .text(function (d) {return d})
+            legend.append("text")
+                .attr("x", width + margin.right - 50 - 8)
+                .attr("y", function (d, i) {return (i * 20) + 9;})
+                .data(unique_category_array)
+                .text(function (d) {return d});
 
-        // Add a box with matching color to the lines for the legend
-        var dataRect = d3.nest()
-            .key(function(d) {return d.category_value;})
-            .entries(unique_category_array);
+            // Add a box with matching color to the lines for the legend
+            var dataRect = d3.nest()
+                .key(function(d) {return d.category_value;})
+                .entries(unique_category_array);
 
-        dataRect.forEach(function(d) {
-            //  console.log(d);
-            legend.append("rect")
-                .attr("x", width + margin.right - 10 - 20)
-                .attr("y", function (d, i) {return i * 20;})
-                .attr("width", 40)
-                .attr("height", 10)
-                .style("fill", function (d) {return color(d) ; })
-        })
+            dataRect.forEach(function(d) {
+                legend.append("rect")
+                    .attr("x", width + margin.right - 110 - 20)
+                    .attr("y", function (d, i) {return i * 20;})
+                    .attr("width", 10)
+                    .attr("height", 10)
+                    .style("fill", function (d) {return color(d) ; })
+            });
 
     });
+
+    // Define tool tip table for charts
+    let tool_tip_chart = d3.tip()
+        .attr("class", "chart-tip")
+        .offset([-8, 0])
+        .html(function (d) {
+                let html = "<table>"
+                    + "<tr>Year : </td>" + d.year +"</td></tr>"
+                    + "<tr><th> "+d.category_value+"</th><td>"+": " +d.avg_data_value+ " cases/10 M" +"</td></tr>"
+                return html
+            });
+    svg.call(tool_tip_chart)
 
     // Define the div for the tooltip
     const div = d3
@@ -252,6 +313,7 @@ function updateGraphs(selectedStates, selectedCategory){
         .append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
+
 
     // Add dots to the line chart
     svg
@@ -266,49 +328,44 @@ function updateGraphs(selectedStates, selectedCategory){
         .attr("stroke", "rgba(0,0,0,0)")
         .style("cursor", "pointer")
         .attr("id","current_dots")
-        .on("mouseover", d => {
-            div
-                .transition()
-                .duration(200)
-                .style("opacity", 0.9);
-            div
-                .html(d.year + "<br/>" + d.avg_data_value)
-                .style("left", d3.event.pageX + "px")
-                .style("top", d3.event.pageY - 28 + "px");
-        })
-        .on("mouseout", () => {
-            div
-                .transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+        .on("mouseover", tool_tip_chart.show)
+        .on("mouseout", tool_tip_chart.hide);
 
     d3.select("#lineChart-radioInputs").style("display", "block");
 
     d3.selectAll(("input[name='btn']")).on("change", function() {
-        //d3.selectAll("#current_factor").remove();
-        //d3.selectAll("#current_dots").remove();
         selectedCategory = this.value
         updateLines(selectedStates, selectedCategory)
-        //console.log("selectedCategory = " + selectedCategory)
-        //console.log("selectedStates = " +selectedStates);
     });
-
+/**
+ * Responsible for updated chart lines based on current radio button selection.
+ * @param selectedStates, selectedCategory
+ **/
 
 function updateLines(selectedStates, selectedCategory){
 
     d3.selectAll("#current_factor").remove();
     d3.selectAll("#current_dots").remove();
     d3.selectAll("#current_legend").remove();
+    d3.selectAll(".no-data").remove();
 
 
     let j = 0;
     while (j< selectedStates.length) {
 
         currentState = selectedStates[j];
-        //console.log(currentState);
-
         statedata = data[2].filter(function(d) {return d.category == selectedCategory && d.locationdesc == currentState;});
+
+        console.log(statedata.length);
+    if (statedata.length == 0){
+        d3.select("#"+ currentState.replace(" ", "-")).select("g").select("g")
+        .append("text")
+        .attr("class", "no-data")
+        .attr("x", 320 / 2)
+        .attr("dy", "5em")
+        .attr("text-anchor", "middle")
+        .text("No data reported for this category");
+    }
 
         var valueline = d3.line()
             .x(function(d) {return x(d.year); })
@@ -318,11 +375,10 @@ function updateLines(selectedStates, selectedCategory){
             .key(function(d) { return d.category_value;})
             .entries(statedata);
 
-        legendSpace = width/dataNest.length; // spacing for the legend
 
-        dataNest.forEach(function(d)  {  //{ console.log(d);
-            //svg=document.getElementById(currentState);
-            d3.select("#"+ currentState)
+
+        dataNest.forEach(function(d)  {
+            d3.select("#"+ currentState.replace(" ", "-"))
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
@@ -334,21 +390,24 @@ function updateLines(selectedStates, selectedCategory){
                 .attr("d", valueline(d.values))
                 .attr("id","current_factor");
 
+
+            legendSpace = width/dataNest.length; // spacing for the legend
+
             //filter the category values for the legend
             let array_of_categories_for_state = statedata.filter(function(d) {return d.locationdesc === currentState;})
-                .map(function(d) { return d.category_value});
+            .map(function(d) { return d.category_value});
 
             //Make a unique list of the category values to remove repeated values
             let unique_category_set = new Set(array_of_categories_for_state);
             let unique_category_array = [...unique_category_set]
 
             //Add in the legend data with category values
-            var legend = svg.selectAll("g.legend")
-                .data(unique_category_array)
-                .enter()
-                .append("g")
-                .attr("class", "legend")
-                .attr("id","current_legend");
+            var legend = d3.select("#"+ currentState.replace(" ", "-")).select("g").selectAll("g.legend")
+                   .data(unique_category_array)
+                   .enter()
+                   .append("g")
+                   .attr("class", "legend")
+                   .attr("id","current_legend");
 
             legend.append("text")
                 .attr("x", width + margin.right - 50 - 8)
@@ -362,15 +421,27 @@ function updateLines(selectedStates, selectedCategory){
                 .entries(unique_category_array);
 
             dataRect.forEach(function(d) {
-                //  console.log(d);
                 legend.append("rect")
                     .attr("x", width + margin.right - 110 - 20)
                     .attr("y", function (d, i) {return i * 20;})
                     .attr("width", 10)
                     .attr("height", 10)
                     .style("fill", function (d) {return color(d) ; })
-            })
+            });
+
         });
+
+        // Define tool tip table for charts
+        let tool_tip_chart = d3.tip()
+            .attr("class", "chart-tip")
+            .offset([-8, 0])
+            .html(function (d) {
+                let html = "<table>"
+                    + "<tr>Year : </td>" + d.year +"</td></tr>"
+                    + "<tr><th> "+d.category_value+"</th><td>"+": " +d.avg_data_value+ " cases/10 M" +"</td></tr>"
+                return html
+            });
+        svg.call(tool_tip_chart)
 
         // Define the div for the tooltip
         const div = d3
@@ -380,7 +451,7 @@ function updateLines(selectedStates, selectedCategory){
             .style("opacity", 0);
 
         // Add the scatterplot
-        d3.select("#"+ currentState)
+        d3.select("#"+ currentState.replace(" ", "-"))
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
@@ -396,22 +467,8 @@ function updateLines(selectedStates, selectedCategory){
             .attr("stroke", "rgba(0,0,0,0)")
             .style("cursor", "pointer")
             .attr("id","current_dots")
-            .on("mouseover", d => {
-                div
-                    .transition()
-                    .duration(200)
-                    .style("opacity", 0.9);
-                div
-                    .html(d.year + "<br/>" + d.avg_data_value)
-                    .style("left", d3.event.pageX + "px")
-                    .style("top", d3.event.pageY - 28 + "px");
-            })
-            .on("mouseout", () => {
-                div
-                    .transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            });
+            .on("mouseover", tool_tip_chart.show)
+            .on("mouseout", tool_tip_chart.hide);
         j++;
     }
 }
